ea618821cda1618135791ddf1773a1ff0947a0c9
lanyangyang758 <caoyanchun90@163.com>
2024-08-19 09:15:03 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -29,8 +29,8 @@ A_CRUISE_MAX_VALS = [1.2, 1.2, 1.1, 1.0, .71, .59, .45, .32, .20, .085]
 A_CRUISE_MAX_BP = [0., 1., 6., 8., 11., 15., 20., 25., 30., 55.]
 
 # Lookup table for turns
-_A_TOTAL_MAX_V = [0.7, 0.5]
-_A_TOTAL_MAX_BP = [20., 40.]
+_A_TOTAL_MAX_V = [.68,  .53,  .36,  .28,  .090]
+_A_TOTAL_MAX_BP = [15.,  20.,  25.,  30.,  55.]
 
 
 EventName = car.CarEvent.EventName
