98d01b6072cb5c9f24316ee7fcb77b3c345219af
lanyangyang758 <caoyanchun90@163.com>
2024-09-03 15:13:56 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -32,7 +32,7 @@ A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 
 # Lookup table for turns
-_A_TOTAL_MAX_V = [0.86, .45,  .36,  .32,  .09]
+_A_TOTAL_MAX_V = [0.92, .53,  .36,  .32,  .09]
 _A_TOTAL_MAX_BP = [11.,  20.,  25.,  30.,  40.]
 
 
