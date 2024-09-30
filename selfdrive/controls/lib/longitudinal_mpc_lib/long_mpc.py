9ecf9e8b99b5855e342beb624f06616a078714e0
lanyangyang758 <caoyanchun90@163.com>
2024-09-30 13:09:06 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -74,13 +74,13 @@ def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
 
 def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    return 1.75
+    return 1.80
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    return 1.45
+    return 1.55
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 1.25
+    return 1.35
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 1.0
+    return 1.20
   elif personality==custom.LongitudinalPersonalitySP.overtake:
     return 0.25
   else:
